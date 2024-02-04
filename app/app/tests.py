d015ff9f68b0be90755d5a499ba99c6a0a579403
Ece Savran <ecesvrn00@gmail.com>
2024-02-04 20:43:14 +0300

    wait for db

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,7 +6,7 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         res = calc.test_subtract_numbers(10, 15)
         self.assertEqual(res, 5)
