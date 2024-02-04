778d1cf393a400638b4e6178844b8f7173014a60
Ece Savran <ecesvrn00@gmail.com>
2024-02-04 20:24:22 +0300

    wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -7,5 +7,6 @@ def add(x, y):
     """Add x and y and return result."""
     return x + y
 
-def test_subtract_numbers(x,y):
+
+def test_subtract_numbers(x, y):
     return y-x
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,11 @@
 from django.test import SimpleTestCase
 from app import calc
+
+
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         res = calc.add(5,6)
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
     def test_subtract_numbers(self):
-        res=calc.test_subtract_numbers(10,15)
+        res=calc.test_subtract_numbers(10, 15)
         self.assertEqual(res,5)
